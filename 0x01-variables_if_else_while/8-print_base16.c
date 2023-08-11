daf7bfe32bed07af159650cc5dcc4397e3e2f6d6
Abdessamad El Allali <abel-all@e2r6p12.1337.ma>
2023-08-11 11:35:52 +0100

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - display alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int	i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
