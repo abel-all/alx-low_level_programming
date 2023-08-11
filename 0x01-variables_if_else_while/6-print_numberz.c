151092d80f8b91f7787849633e0c38d3e77c535c
Abdessamad El Allali <abel-all@e2r6p12.1337.ma>
2023-08-11 11:32:51 +0100

    print int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - display alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int	i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
