1a28ce2d67ca43dcb59a7d3574da50f40f6d68af
Abdessamad El Allali <abel-all@e2r6p12.1337.ma>
2023-08-11 10:47:36 +0100

    rand

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - function assign random value to n and display what is sign of this number
+ * 
+ * Return: is always 0 (sucsess)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
