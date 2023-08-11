69b81ecad25d8e27929643d3c7715b16ad13408c
Abdessamad El Allali <abel-all@e2r6p12.1337.ma>
2023-08-11 11:28:54 +0100

    print alphapt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - display alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int	i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
