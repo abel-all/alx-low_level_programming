e779bd35c2c2062067adebcb9ec6eac950607eba
Abdessamad El Allali <abel-all@e3r7p4.1337.ma>
2023-08-15 20:55:58 +0100

    alph*10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - program that display _putchar
+ *
+ * Return: no return value void
+ */
+
+void print_alphabet(void)
+{
+	int	i;
+
+	i = 'a';
+	while (i <= 'z')
+		_putchar(i++);
+	_putchar('\n');
+}
+
+void print_alphabet_x10(void)
+{
+	int	i;
+
+	i = -1;
+	while (++i < 10)
+		print_alphabet();
+}
