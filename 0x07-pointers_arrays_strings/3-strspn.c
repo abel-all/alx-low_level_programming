2486eea14c0bb120867a63422511243482ac543f
Abdessamad El Allali <abel-all@e3r7p2.1337.ma>
2023-08-28 13:14:37 +0100

    finish cpp 00 expect time func

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+/**
+ * inCharSet - checks if a character is in a set of characters
+ * @c: character to be checked
+ * @accept: string containing the characters to match
+ * Return: 1 if c is in accept, 0 otherwise
+ */
+int _inCharSet(char c, char *accept)
+{
+	int i = 0;
+
+	while (accept[i])
+		if (c == accept[i++])
+			return (1);
+	return (0);
+}
+/**
+ * _strspn - prints buffer in hexa
+ * @s: the char pointer
+ * @accept: the char
+ *
+ * Return: the final uint.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	while (s[i] && _inCharSet(s[i], accept))
+		i++;
+	return (i);
+}
