c0a3794a4ef2025bb02d2d065779effbde6a975b
Abdessamad El Allali <abel-all@e3r7p2.1337.ma>
2023-08-28 13:31:50 +0100

    finish cpp 00 expect time func

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+/**
+ * print_diagsums - gets the length of a prefix substring
+ * @a: string containing the characters to match
+ * @size: string containing the characters to match
+ *
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * (size + 1)];
+		sum2 += a[(i + 1) * (size - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
