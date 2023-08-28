8a66802509fc9fb1b32125c858f0d6160a3db71f
Abdessamad El Allali <abel-all@e3r7p2.1337.ma>
2023-08-28 13:34:41 +0100

    finish cpp 00 expect time func

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+
+/**
+ * set_string - gets the length of a prefix substring
+ * @to: string containing the characters to match
+ * @s: string containing the characters to match
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
